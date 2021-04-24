ce13395219406e39bad396db03a9948ffc04a8d0
rwm5661 <78108142+rwm5661@users.noreply.github.com>
2021-04-24 17:44:16 -0400

    Login and Mainframe updated

---
+++ src/java/SharedClasses/TrendsDataCalculator.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SharedClasses;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author RIVER
+ */
+public class TrendsDataCalculator {
+    public static ArrayList<Dataset> calculateTrendsData(ArrayList<Dataset> selectedMonth, ArrayList<Dataset> previousMonth, String month) {
+        ArrayList<Dataset> trendsData = new ArrayList<Dataset>();
+        return trendsData;
+    }
+}
